009068320cc9a915ba86db571d513b23439fde0a
Chris Blazej <cblazej@ucsd.edu>
2018-11-29 16:25:42 -0800

    Added pictureTaker.py for Rpi, added my pictures to /images/ (42)

--- project_2/pictureTaker.py
+++ project_2/pictureTaker.py
@@ -1,12 +1,9 @@
 """
 ECE196 Face Recognition Project
-Author: W Chen
+Author: Chris Blazej
 
 Adapted from:
 http://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
-
-Use this code as a template to process images in real time, using the same techniques as the last challenge.
-You need to display a gray scale video with 320x240 dimensions, with box at the center
 """
 
 
@@ -15,47 +12,52 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-
-def process_image(img, newWidth, newHeight):
-    # Converts the image to grayscale.
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Resizes our image to our newly scaled dimensions
-    img = cv2.resize(img, (newWidth, newHeight), interpolation = cv2.INTER_AREA)
-
-    cv2.rectangle(img,(newWidth/2 - 50,newHeight/2 - 50),(newWidth/2 + 50,newHeight/2 + 50),(255,255,255),2)
-            
-    return img
+import os
+import os.path
 
 def main():
     # initialize the camera and grab a reference to the raw camera capture
     camera = PiCamera()
-    camera.resolution = (800, 600)
+    camera.resolution = (800, 608)
     camera.framerate = 24
-    rawCapture = PiRGBArray(camera, size=(800, 600))
+    rawCapture = PiRGBArray(camera, size=(800, 608))
 
+    if(not os.path.exists("./images/")):
+        os.mkdir("images")
+
+    print("Taking pictures in:")
+
+    for i in list(reversed(range(1,6))):
+        print(i)
+        time.sleep(1)
+    
     frameCount = 0
-    # allow the camera to warmup
-    time.sleep(0.1)
-  
+    pictureNum = 0
+    
     # capture frames from the camera
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
         image = frame.array
 
-        image = process_image(image, 160, 120)
-  
+        if (frameCount >= 6):
+            cv2.imwrite("./images/" + str(pictureNum) + '.jpg', image)
+            print("./images/" + str(pictureNum) + ".jpg written")
+            pictureNum = pictureNum + 1
+            frameCount = 0
+        else:
+            frameCount = frameCount + 1
+        
         # show the frame
         cv2.imshow("Frame", image)
         key = cv2.waitKey(1) & 0xFF
-  
+                
         # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
-  
+        
         # if the `q` key was pressed, break from the loop
-        if (key == ord("q")):
+        if key == ord("q"):
             break
-      
+                
 if (__name__ == "__main__"):
-  main()
+    main()
